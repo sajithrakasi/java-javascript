49472d28b369bfe62cd5b7e03f7a2b1973582fd6
sajithra kasi <120911181+sajithrakasi@users.noreply.github.com>
2023-07-01 12:08:03 +0530

    javascript

---
+++ intro.js
@@ -0,0 +1,104 @@
+ //single line comment /*multi line comment*/
+        //this is alert
+//dynamicaly typed language
+        //variables -8 bytes (64 bits)
+       // let score=0 //initialization
+        //assignment
+        //score=10.5 //binary 1010
+        //score='no score'
+
+        //const pi=3.14
+        
+      // console.log("score is:" +score);
+
+//arithmetic operators: + - ** / % ++ --
+//assignment opertaor: = += -= *= /= %= **=
+//comparison operators: == === != !== > <= >= ?:
+//logical:&& || !
+//bitwise operators: & | ^  << >> >>>
+
+//user input
+// let name=prompt("what is your name?")
+// console.log("hi:",name);
+
+// let tickets=prompt("how many tickets do you want?")
+// console.log('you should pay:'+tickets*20);
+
+
+//let a=prompt("enter a  number")
+//console.log(Number(a)+10)
+
+
+
+// Template literals
+
+let firstName = "Ramya"
+let lastName="Murali"
+let city="coimbatore"
+
+console.log(firstName+" "+lastName+" lives in "+city)
+
+//backtick & string interpolation
+ console.log(`${firstName} ${lastName} lives in ${city}`)
+
+//multiline string
+let msg=`happy birthday`
+console.log(msg)
+
+//single and double quotes in string
+msg="cat's name is toto"
+msg=`cat's name is "toto"`
+
+console.log(msg)
+
+let item1="rose,"
+let item2="sunflower,"
+let item3="jasmine,"
+let item4="lotus,"
+let item5="lily"
+
+console.log(item1+" "+item2+" "+item3+" "+item4+" "+item5)
+
+ message=`"you have 5 items in your cart your bil amount is 95"`
+
+console.log(message)
+
+
+//arrays
+
+let num=10
+let name="saji";
+let cities=["chennai","madurai","trichy"];
+
+let marks=[78,23,45,75,12];
+
+//length-total elements in the array
+
+console.log(cities.length)
+
+//exercise
+
+arr=[5,8,10,7,9,11]
+
+arr.splice(3,3,17,19,111)
+arr.unshift(100)
+arr.push(1000)
+arr.splice(3,0,0)
+
+console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
